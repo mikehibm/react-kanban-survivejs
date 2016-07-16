591c2ec9de3da9920ca0f9b8445e0d61b4d97eb6
Makoto Ishida <makotoishida@gmail.com>
2016-07-16 12:17:36 -1000

    Setting up webpack.

---
+++ app/index.js
@@ -0,0 +1,6 @@
+require('./main.css');
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+app.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
+};
+
+const common = {
+	entry: {
+		app: PATHS.app
+	},
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: PATHS.app
+			}
+		]
+	}
+};
+
+//Default config
+if (TARGET === 'start' || !TARGET) {
+	module.exports = merge(common, {
+		devtool: 'eval-source-map',
+		devServer: {
+			contentBase: PATHS.build,
+
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+
+			stats: 'errors-only',
+
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+			new NpmInstallPlugin({
+				save: true
+			})
+		]
+	});
+}
+
+if (TARGET === 'build') {
+	module.exports = merge(common, {});
+}

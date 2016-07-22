ea0e62fd56ab98d3dcf3415c0c54c41c38311727
Makoto Ishida <makotoishida@gmail.com>
2016-07-22 10:27:39 -1000

    Add Edit note function.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,32 +3,47 @@ import React from 'react';
 import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			notes: [
-					{ id: uuid.v4(), task: 'Learn React' },
-					{ id: uuid.v4(), task: 'Learn Webpack' }
-				]
-		};
+    this.state = {
+      notes: [
+          { id: uuid.v4(), task: 'Learn React' },
+          { id: uuid.v4(), task: 'Learn Webpack' }
+        ]
+    };
 
-	}
+  }
 
-	render() {
-		const notes = this.state.notes;
+  render() {
+    const notes = this.state.notes;
 
-		return (
-			<div>
-				<button onClick={this.addNote}>+</button>
-				<Notes notes={notes} />
-			</div>
-		);
-	}
+    return (
+      <div>
+        <h3>My Notes</h3>
+        <button onClick={this.addNote}>+ Add</button>
+        <Notes notes={notes} onEdit={this.editNote} />
+      </div>
+    );
+  }
 
-	addNote = () => {
-		this.setState({
-			notes: [...this.state.notes, { id: uuid.v4(), task: 'New task'}]
-		});
-	};
+  addNote = () => {
+    this.setState({
+      notes: [...this.state.notes, { id: uuid.v4(), task: 'New task'}]
+    });
+  };
+
+  editNote = (id, task) => {
+    if (!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if (note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({ notes });
+  };
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Note from './Note.jsx';
 
-export default ({notes}) => {
-	return (
-		<ul>{ notes.map(note =>
-			<li key={note.id}>
-				<Note task={note.task} />
-			</li>
-		)}
-		</ul>
-	);
+export default ({notes, onEdit}) => {
+  return (
+    <ul>{ notes.map(note =>
+      <li key={note.id}>
+        <Note task={note.task}
+            onEdit={ onEdit.bind(null, note.id) }/>
+      </li>
+    )}
+    </ul>
+  );
 };

68bcb00d7348fb0925a15e1d5695c5e31288d8bf
Makoto Ishida <makotoishida@gmail.com>
2016-07-22 00:19:21 -1000

    Setup VS Code, JSHint and ESLint etc.

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 
 export default ( {task} ) =>
-	<div>{task}</div>;
+	<div>
+		{task}
+	</div>;
--- app/index.jsx
+++ app/index.jsx
@@ -4,12 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-// var component = require('./component');
-// var app = document.createElement('div');
-
-// document.body.appendChild(app);
-// app.appendChild(component());
-
-//setTimeout(() => console.log('ES2015 test.'), 1000);

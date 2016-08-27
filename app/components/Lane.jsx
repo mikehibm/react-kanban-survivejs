b7fcb1cfe437cff4fc0cc73f7f28c76f24a8c377
Makoto Ishida <makotoishida@gmail.com>
2016-08-27 09:32:18 -1000

    Refactor using Lane.Header/Lane.Notes namespace.

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -12,32 +12,24 @@ export default class Lane extends React.Component {
     const { lane, ...props } = this.props;
     return (
       <div className="lane" {...props} >
-        <div className="lane-header" onClick={this.activateLaneEdit}>
-          <div className="lane-add-note">
-            <button onClick={this.addNote}>+</button>
-          </div>
-          <button className="lane-delete" onClick={this.deleteLane}>x</button>
-          <Editable className="lane-name" editing={lane.editing} value={lane.name} onEdit={this.editName} />
-        </div>
-        <AltContainer
-          stores={[NoteStore]}
-          inject={{ notes: () => NoteStore.getNotesByIds(lane.notes) }}
-          >
-          <Notes
-            onValueClick={this.activateNoteEdit}
-            onEdit={this.editNote}
-            onDelete={this.deleteNote} />
-        </AltContainer>
       </div>
     );
   }
 
-  editNote(id, task) {
-    if (!task.trim()) {
-      NoteActions.update({id, editing: false});
-      return;
-    }
-    NoteActions.update({id, task, editing: false});
+}
+
+Lane.Header = class LaneHeader extends React.Component {
+  render() {
+    const lane = this.props.lane;
+    return (
+      <div className="lane-header" onClick={this.activateLaneEdit}>
+        <div className="lane-add-note">
+          <button onClick={this.addNote}>+</button>
+        </div>
+        <button className="lane-delete" onClick={this.deleteLane}>x</button>
+        <Editable className="lane-name" editing={lane.editing} value={lane.name} onEdit={this.editName} />
+      </div>
+    );
   }
 
   addNote = (e) => {
@@ -49,13 +41,6 @@ export default class Lane extends React.Component {
       noteId: note.id,
       laneId
     });
-  };
-
-  deleteNote = (noteId, e) => {
-    e.stopPropagation();
-    const laneId = this.props.lane.id;
-    LaneActions.detachFromLane({laneId, noteId});
-    NoteActions.delete(noteId)
   };
 
   editName = (name) => {
@@ -77,8 +62,41 @@ export default class Lane extends React.Component {
     LaneActions.update({id: laneId, editing: true});
   };
 
+};
+
+Lane.Notes = class LaneNotes extends React.Component {
+  render() {
+    const lane = this.props.lane;
+    return (
+      <AltContainer
+        stores={[NoteStore]}
+        inject={{ notes: () => NoteStore.getNotesByIds(lane.notes) }}
+        >
+        <Notes
+          onValueClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote} />
+      </AltContainer>
+    );
+  }
+
   activateNoteEdit = (id) => {
     NoteActions.update({id, editing: true});
   };
 
-}
+  editNote(id, task) {
+    if (!task.trim()) {
+      NoteActions.update({id, editing: false});
+      return;
+    }
+    NoteActions.update({id, task, editing: false});
+  }
+
+  deleteNote = (noteId, e) => {
+    e.stopPropagation();
+    const laneId = this.props.lane.id;
+    LaneActions.detachFromLane({laneId, noteId});
+    NoteActions.delete(noteId)
+  };
+
+};
